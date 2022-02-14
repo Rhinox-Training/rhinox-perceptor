eb7d32ffa090c1281a4eb63e391ee5c24179de6c
SPIRALGaetan <gaetan@rhinox.training>
2022-02-14 17:43:54 +0100

    Added samples, package.json and AssemblyDefinition files

---
+++ Samples~/Demo/Main.cs
@@ -0,0 +1,22 @@
+﻿using Rhinox.Perceptor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Samples
+{
+    
+    public static class Main
+    {
+        [RuntimeInitializeOnLoadMethod]
+        public static void Start()
+        {
+            Rhinox.Perceptor.PLog.CreateIfNotExists();
+        }
+
+        [MenuItem("Test/TestLogin")]
+        public static void LogTest()
+        {
+            PLog.Info<MyCustomLogger>($"Test log from MenuItem");
+        }
+    }
+}
