cba5ea5f674c0a74680c74ba2342a0dba9a5c334
SPIRAL\Gaetan <gaetan@rhinox.training>
2022-02-21 11:20:49 +0100

    refactor

--- Assets/Perceptor/Samples~/Demo/MyCustomLogger.cs
+++ Assets/Perceptor/Samples~/Demo/MyCustomLogger.cs
@@ -2,8 +2,13 @@
 
 namespace Samples
 {
-    public class MyCustomLogger : BaseLogger
+    public class MyCustomLogger : CustomLogger
     {
-        
+        public override void SetupTargets()
+        {
+            _logTargets.Add(FileLogTarget.CreateByName("custom"));
+            _logTargets.Add(FileLogTarget.CreateByPath("../Logs/custom.log"));
+            _logTargets.Add(new UnityLogTarget());
+        }
     }
 }
