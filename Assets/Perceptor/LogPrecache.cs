8f8bc2e60ee80985c5f5206db09c12e19b0b9214
Gaetan Deglorie <gaetan@rhinox.training>
2022-04-29 12:18:59 +0200

    Fixes

--- Assets/Perceptor/LogPrecache.cs
+++ Assets/Perceptor/LogPrecache.cs
@@ -11,6 +11,13 @@ namespace Rhinox.Perceptor
             public LogLevels Levels;
             public string Message;
             public UnityEngine.Object AssociatedObject;
+
+            public LogEntryCache(LogLevels levels, string message, UnityEngine.Object associatedObject)
+            {
+                Levels = levels;
+                Message = message;
+                AssociatedObject = associatedObject;
+            }
         }
 
         private Dictionary<Type, List<LogEntryCache>> _cache;
@@ -36,14 +43,18 @@ namespace Rhinox.Perceptor
                 _cache.Add(loggerType, new List<LogEntryCache>());
             var list = _cache[loggerType];
             if (list.Count >= _entryLimitPerType)
-                return false;
-            
-            list.Add(new LogEntryCache()
             {
-                Levels = levels,
-                Message = message,
-                AssociatedObject = associatedObject
-            });
+                if (list.Count == _entryLimitPerType)
+                {
+                    list.Add(new LogEntryCache(LogLevels.Warn,
+                        $"The cache for logger of type '{loggerType?.Name}' has overflowed.", null));
+                    
+                    _cache[loggerType] = list;
+                }
+                return false;
+            }
+
+            list.Add(new LogEntryCache(levels, message, associatedObject));
             _cache[loggerType] = list;
             return true;
         }
@@ -53,19 +64,35 @@ namespace Rhinox.Perceptor
         {
             if (_cache == null || _cache.Count == 0)
                 return;
-            
-            foreach (var logger in loggers)
+
+            UnityLogTarget.Silence = true; // NOTE: UnityLogging is already handled while caching, this prevents logging to Unity twice
+            try
             {
-                if (logger == null)
-                    continue;
-                var loggerType = logger.GetType();
-                if (_cache.ContainsKey(loggerType))
+                foreach (var logger in loggers)
                 {
-                    foreach (var entry in _cache[loggerType])
-                        logger.Log(entry.Levels, entry.Message, entry.AssociatedObject);
-                    _cache[loggerType].Clear();
+                    if (logger == null)
+                        continue;
+                    var loggerType = logger.GetType();
+                    if (_cache.ContainsKey(loggerType))
+                    {
+                        foreach (var entry in _cache[loggerType])
+                            logger.Log(entry.Levels, entry.Message, entry.AssociatedObject);
+                        _cache[loggerType].Clear();
+                    }
                 }
             }
+            catch (Exception e)
+            {
+                Debug.LogError($"Something went wrong during FlushCache: " + e.ToString());
+            }
+
+            UnityLogTarget.Silence = false;
+
+            _cache.Clear();
+        }
+
+        public void Clear()
+        {
             _cache.Clear();
         }
     }
